ed90a9ba76b84b71f8b189680d99d54859a02c8b
Waya-mst <kameeeeei1234@gmail.com>
2024-03-24 02:50:06 +0900

    add class, construct, member func

--- windowmake/windowmake/game.cpp
+++ windowmake/windowmake/game.cpp
@@ -1,1 +1,52 @@
-#include<iostream>
+#include "game.h"
+
+Game::Game()
+:mWindow(nullptr)
+, mIsRunning(nullptr)
+{
+
+}
+
+bool Game::Initialize()
+{
+	int sdlResult = SDL_Init(SDL_INIT_VIDEO);
+
+	if (sdlResult != 0) {
+		SDL_Log("SDLを初期化できません: %s", SDL_GetError());
+		return false;
+	}
+
+	mWindow = SDL_CreateWindow(
+		"Window make",
+		100,	// ウィンドウ左上隅のx座標
+		100,	// ウィンドウ左上隅のY座標
+		1024,	// Width
+		768,	// height
+		SDL_WINDOW_RESIZABLE	// フラグ
+	);
+
+	if (!mWindow) {
+		SDL_Log("ウィンドウの作成に失敗: %s", SDL_GetError());
+		return false;
+	}
+}
+
+void Game::RunLoop()
+{
+	while (mIsRunning) {
+		ProcessInput();
+		UpdateGame();
+		GenerateOutput();
+	}
+}
+
+void Game::Shutdown()
+{
+	SDL_DestroyWindow(mWindow);
+	SDL_Quit();
+}
+
+void Game::ProcessInput()
+{
+	SDL_Event event;
+}
--- windowmake/windowmake/game.h
+++ windowmake/windowmake/game.h
@@ -5,5 +5,17 @@ class Game
 {
 public:
 	Game();
+	bool Initialize();
+	void RunLoop();
+	void Shutdown();
+private:
+	// RunLoop内で使われる関数
+	void ProcessInput();
+	void UpdateGame();
+	void GenerateOutput();
 
+	// SDLで作られるウィンドウ
+	SDL_Window* mWindow;
+	// ゲームの続行を示すフラグ
+	bool mIsRunning;
 };
